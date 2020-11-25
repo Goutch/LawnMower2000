a1da2e9bb332758568f6467eeb556de55e88f9ff
Gabriel Bouchard <gaboutch@outlook.com>
2020-11-25 15:38:25 -0500

    TitleScreen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,11 +15,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject lawnMowerPrefab;
     [SerializeField] private GameObject gameMenuPrefab;
     [SerializeField] private float gameDuration;
-
+    [SerializeField] private GameObject titleScreenPrefab;
     private Options options;
     private bool gameSceneActive = false;
     private Map map;
     private bool inGameMenuIsOpened = false;
+    private bool titleScreenShowed = false;
 
     public delegate void OnGameStartHandler();
 
@@ -64,7 +65,7 @@ public class GameManager : MonoBehaviour
             OnGameTimeChange?.Invoke(gameTime);
         }
 
-        if(GameFinished)
+        if (GameFinished)
         {
             FinishGame();
         }
@@ -119,6 +120,7 @@ public class GameManager : MonoBehaviour
     {
         options = GetComponent<Options>();
         SceneManager.sceneLoaded += OnSceneLoaded;
+
         LoadMenu();
     }
 
@@ -168,7 +170,6 @@ public class GameManager : MonoBehaviour
         networkManager.OnJoinedRoomEvent -= OnJoinedRoom;
 
         StartNetworkGame();
-        
     }
 
     public bool IsGameActive()
@@ -205,6 +206,12 @@ public class GameManager : MonoBehaviour
         if (scene.name == "Menu")
         {
             SceneManager.SetActiveScene(scene);
+            if (!titleScreenShowed)
+            {
+                titleScreenShowed = true;
+                Instantiate(titleScreenPrefab,Vector3.zero,Quaternion.identity);
+            }
+
             if (IsGameActive())
             {
                 SceneManager.UnloadSceneAsync("Game");

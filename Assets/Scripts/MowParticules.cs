6215681407415260511f11a0eea05db25d697927
Gabriel Bouchard <gaboutch@outlook.com>
2020-12-01 13:41:55 -0500

    Update MowParticules.cs

--- Assets/Scripts/MowParticules.cs
+++ Assets/Scripts/MowParticules.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using UnityEditor;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Matrix4x4 = UnityEngine.Matrix4x4;
@@ -12,7 +8,7 @@ using Vector4 = UnityEngine.Vector4;
 
 public class MowParticules : MonoBehaviour
 {
-    [SerializeField] private float maxParticules = 100;
+    [SerializeField] private int maxParticules = 100;
     [SerializeField] float particuleLifeTime = 2;
     [SerializeField] private float spawnRatePerSeconds = 0.1f;
     [SerializeField] private float initialSpeed = 0.5f;
@@ -23,13 +19,14 @@ public class MowParticules : MonoBehaviour
     private Material material;
     private LawnMower lawnMower;
     private Mesh quad;
-    private List<Matrix4x4> particules;
-    private List<Vector4> colors;
-    private List<Vector4> velocities;
-    private List<float> timeToLive;
+    private Matrix4x4[] particules;
+    private Vector4[] colors;
+    private Vector4[] velocities;
+    private float[] timeToLive;
     private GameManager gameManager;
     MaterialPropertyBlock colorsProperty;
     private Random rnd;
+    private int currentParticulesCount = 0;
 
     private void OnEnable()
     {
@@ -42,10 +39,10 @@ public class MowParticules : MonoBehaviour
 
         quad = GeometryUtils.CreateQuad(1.0f / gameManager.Map.PixelsPerUnits, 1.0f / gameManager.Map.PixelsPerUnits);
         gameManager.OnGameStart += OnGameStart;
-        particules = new List<Matrix4x4>();
-        velocities = new List<Vector4>();
-        timeToLive = new List<float>();
-        colors = new List<Vector4>();
+        particules = new Matrix4x4[maxParticules];
+        velocities = new Vector4[maxParticules];
+        timeToLive = new float[maxParticules];
+        colors = new Vector4[maxParticules];
         colorsProperty = new MaterialPropertyBlock();
     }
 
@@ -61,45 +58,40 @@ public class MowParticules : MonoBehaviour
 
     private void Update()
     {
-        for (int i = particules.Count - 1; i >= 0; i--)
+        for (int i = 0; i < currentParticulesCount; i++)
         {
             timeToLive[i] -= Time.deltaTime;
             if (timeToLive[i] <= 0)
             {
-                particules.RemoveAt(i);
-                timeToLive.RemoveAt(i);
-                velocities.RemoveAt(i);
-                colors.RemoveAt(i);
+                currentParticulesCount--;
+                particules[i] = particules[currentParticulesCount];
+                timeToLive[i] = timeToLive[currentParticulesCount];
+                velocities[i] = velocities[currentParticulesCount];
+                colors[i] = colors[currentParticulesCount];
             }
-            else if (particules[i].m23 < minZ)
+        }
+
+        for (int i = 0; i < currentParticulesCount; i++)
+        {
+            if (particules[i].m23 < minZ)
             {
-                velocities[i] = new Vector4(
-                    velocities[i].x * lossOfMomentum,
-                    velocities[i].y * lossOfMomentum,
-                    velocities[i].z - (gravity * Time.deltaTime),
-                    1);
+                velocities[i].x *= lossOfMomentum;
+                velocities[i].y *= lossOfMomentum;
+                velocities[i].z -= (gravity * Time.deltaTime);
                 Vector4 newPosition = new Vector4(
-                    particules[i].m03,
-                    particules[i].m13,
-                    particules[i].m23,
+                    particules[i].m03 + (velocities[i].x * Time.deltaTime),
+                    particules[i].m13 + (velocities[i].y * Time.deltaTime),
+                    particules[i].m23 + (velocities[i].z * Time.deltaTime),
                     particules[i].m33);
-
-                newPosition.x += velocities[i].x * Time.deltaTime;
-                newPosition.y += velocities[i].y * Time.deltaTime;
-                newPosition.z += velocities[i].z * Time.deltaTime;
-                particules[i] = new Matrix4x4(
-                    new Vector4(1, 0, 0, 0),
-                    new Vector4(0, 1, 0, 0),
-                    new Vector4(0, 0, 1, 0),
-                    newPosition);
+                particules[i].SetColumn(3, newPosition);
             }
+        }
 
-            if (lawnMower.Mowing && particules.Count != 0)
-            {
-                colorsProperty = new MaterialPropertyBlock();
-                colorsProperty.SetVectorArray("_Colors", colors);
-                Graphics.DrawMeshInstanced(quad, 0, material, particules.ToArray(), particules.Count, colorsProperty);
-            }
+        if (lawnMower.Mowing && currentParticulesCount != 0)
+        {
+            colorsProperty = new MaterialPropertyBlock();
+            colorsProperty.SetVectorArray("_Colors", colors);
+            Graphics.DrawMeshInstanced(quad, 0, material, particules, currentParticulesCount, colorsProperty);
         }
     }
 
@@ -108,15 +100,15 @@ public class MowParticules : MonoBehaviour
         while (lawnMower.Mowing)
         {
             yield return new WaitForSeconds(spawnRatePerSeconds);
-            if (particules.Count < maxParticules)
+            if (currentParticulesCount < maxParticules)
             {
-                particules.Insert(0, Matrix4x4.Translate(new Vector3(transform.position.x, transform.position.y, .5f)));
-                velocities.Insert(0, new Vector4(
+                particules[currentParticulesCount] = Matrix4x4.Translate(new Vector3(transform.position.x, transform.position.y, .5f));
+                velocities[currentParticulesCount] = new Vector4(
                     ((rnd.Next(-50, 50) / 100f) * 2) + (-transform.up.x * initialSpeed),
                     (rnd.Next(-50, 50) / 100f) + (-transform.up.y * initialSpeed),
                     -initialUpVelocity,
-                    0));
-                timeToLive.Insert(0, particuleLifeTime);
+                    0);
+                timeToLive[currentParticulesCount] = particuleLifeTime;
                 Color color;
                 if (lawnMower.IsStuck)
                 {
@@ -132,7 +124,8 @@ public class MowParticules : MonoBehaviour
                             front.z));
                 }
 
-                colors.Insert(0, color);
+                colors[currentParticulesCount] = color;
+                currentParticulesCount++;
             }
         }
     }
